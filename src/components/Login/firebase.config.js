8af69046fdb3fdc1f79f6e4de4a48a5426068930
Tanvir Anjum Labir <tanvir.labir@gmail.com>
2022-01-13 15:50:21 +0600

    fixing some deploy related problems

--- src/components/Login/firebase.config.js
+++ src/components/Login/firebase.config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAFTm2iqwKiWMlBqG5d0YgWgnry8nKBVqw",
-    authDomain: "ema-john-simple-3199d.firebaseapp.com",
-    databaseURL: "https://ema-john-simple-3199d.firebaseio.com",
-    projectId: "ema-john-simple-3199d",
-    storageBucket: "ema-john-simple-3199d.appspot.com",
-    messagingSenderId: "351625833299",
-    appId: "1:351625833299:web:719f5ad7a4bb46df5dcfa6"
-  };
+  apiKey: "AIzaSyAFTm2iqwKiWMlBqG5d0YgWgnry8nKBVqw",
+  authDomain: "ema-john-simple-3199d.firebaseapp.com",
+  databaseURL: "https://ema-john-simple-3199d.firebaseio.com",
+  projectId: "ema-john-simple-3199d",
+  storageBucket: "ema-john-simple-3199d.appspot.com",
+  messagingSenderId: "351625833299",
+  appId: "1:351625833299:web:719f5ad7a4bb46df5dcfa6"
+};
 
   export default firebaseConfig;
